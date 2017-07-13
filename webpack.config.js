9824d9e7398234d7498c93267e208910ca8fd03f
Greg Manda <grzegorz.manda@gmail.com>
2017-07-13 02:00:46 +0200

    adding assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
-const ExtractText = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSS = require('optimize-css-assets-webpack-plugin');
 
 let config = {
     entry: './src/index.js',
@@ -17,15 +19,31 @@ let config = {
     devtool: 'eval-source-map',
     module: {
         rules: [
+            { 
+              test: /\.(jpe?g|gif|png|svg)$/i,
+              loaders: [
+                  'file-loader?context=src/assets/images&name=images/[path][name].[ext]',
+                  { 
+                      loader: 'image-webpack-loader',
+                  }
+              ]
+            },
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
             { test: /\.scss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
             // SEPARATE STYLESHEET
-            // { test: /\.scss$/, use: ExtractText.extract({ use: ['css-loader', 'sass-loader'], fallback: 'style-loader' }) }
+            // { test: /\.scss$/, use: ExtractTextPlugin.extract({ use: ['css-loader', 'sass-loader'], fallback: 'style-loader' }) }
         ]
     },
     plugins: [
-        // new ExtractText('styles.css')
+        // new ExtractTextPlugin('styles.css')
     ]
 };
 
 module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeCSS()
+    );
+}
